668fae28d51b1ec094ed888cf7a1053e8038fd15
graphxdesigns2 <graphxdesigns2@gmail.com>
2026-08-05 00:32:02 -0400

    Moved media folder

--- collections/Media.ts
+++ collections/Media.ts
@@ -1,4 +1,5 @@
 import type { CollectionConfig } from 'payload'
+import path from 'path'
 
 export const Media: CollectionConfig = {
   slug: 'media',
@@ -12,5 +13,7 @@ export const Media: CollectionConfig = {
       required: true,
     },
   ],
-  upload: true,
+  upload: {
+    staticDir: path.resolve('/tmp'),
+  },
 }
